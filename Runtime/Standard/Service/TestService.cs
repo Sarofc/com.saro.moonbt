e3cecccf77a5be32f5545b420e82216fbcf99361
Sarofc <sarono@foxmail.com>
2022-12-08 22:05:42 +0800

    整理

--- Runtime/Standard/Service/TestService.cs
+++ Runtime/Standard/Service/TestService.cs
@@ -1,20 +1,20 @@
 ﻿
-using System.Text;
+//using System.Text;
 
-namespace Saro.BT
-{
-    public class TestService : BTService
-    {
-        protected override void ServiceTick()
-        {
-            UnityEngine.Debug.Log("test...");
-        }
+//namespace Saro.BT.Test
+//{
+//    public class TestService : BTService
+//    {
+//        protected override void ServiceTick()
+//        {
+//            UnityEngine.Debug.Log("test...");
+//        }
 
-        public override void Description(StringBuilder builder)
-        {
-            base.Description(builder);
+//        public override void Description(StringBuilder builder)
+//        {
+//            base.Description(builder);
 
-            builder.Append("\ntest service...");
-        }
-    }
-}
+//            builder.Append("\ntest service...");
+//        }
+//    }
+//}
